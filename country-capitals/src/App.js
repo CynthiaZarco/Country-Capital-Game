10184e7d989eac92d077a456bfd353853f1eea7a
CynthiaZarco <cynthia.zarco.h@hotmail.es>
2024-08-14 15:40:59 -0600

    Adding JSON data file and creating Button Component
    - Adding Json data for Countries
    - Create Button Component
    - fetch call to get Json data

--- country-capitals/src/App.js
+++ country-capitals/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React,{useEffect, useState} from 'react';
 import './App.css';
-
+import Button from './components/Button';
 function App() {
+
+ useEffect(()=>{
+    fetch('/data/countries.json',
+      {
+        method:'get'
+      }
+    )
+    .then((result) => result.json())
+    .then((result) =>{
+      console.log(result);
+    })
+ },[]);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Country Capital Match Game
+        <Button></Button>
       </header>
+      
     </div>
   );
 }
---
+++ country-capitals/src/components/Button.js
@@ -0,0 +1,7 @@
+const Button = (props)=>{
+  return(
+    <button>Test</button>
+  )
+}
+
+export default Button;
